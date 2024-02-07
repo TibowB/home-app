e9b6de846c978fceda3f4f2dc36d5d0e25dbf5b4
TibowB <bruggeman.dev@gmail.com>
2024-02-07 17:05:19 +0100

    Redo backend with express js

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # home-app
+
 A personal app for managing your home
+
+# Backend
+
+Build with Express.js
+
+ORM : https://sequelize.org/
+
+CREATE DATABASE home_app
---
+++ back/index.js
@@ -0,0 +1,65 @@
+import express from "express";
+import { Sequelize, DataTypes } from "sequelize";
+import "http-status";
+import httpStatus from "http-status";
+
+const sequelize = new Sequelize(
+  "mysql://root:root123$@localhost:3306/home_app",
+  {
+    host: "localhost",
+    dialect: "mysql",
+  }
+);
+
+const Recipe = sequelize.define("recipe", {
+  name: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
+
+Recipe.sync();
+
+try {
+  await sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/recipes", async (req, res) => {
+  const recipes = await Recipe.findAll();
+
+  res.status(httpStatus.OK).send(recipes);
+});
+
+app.post("/api/recipes", async (req, res) => {
+  try {
+    const recipe = await Recipe.create({ name: req.body.name });
+    await recipe.save();
+    res.sendStatus(httpStatus.CREATED);
+  } catch (error) {
+    console.error("Error saving new recipe", error);
+    res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+});
+
+app.delete("/api/recipes/:id", async (req, res) => {
+  await Recipe.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.sendStatus(httpStatus.NO_CONTENT);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
