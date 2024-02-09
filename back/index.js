ec9cd1ac34f02f0ff397dbece32f794b917743bb
TibowB <bruggeman.dev@gmail.com>
2024-02-09 16:47:04 +0100

    Update cors

--- back/index.js
+++ back/index.js
@@ -34,7 +34,7 @@ await Recipe.sync();
 const app = express();
 const port = 3000;
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
